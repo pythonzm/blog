d47ef74352a634764a21d7d46f110ede15bf0245
pythonzm <zm_world@163.com>
2019-11-12 16:50:16 +0800

    修改db.sql

--- backend/db.sql
+++ backend/db.sql
@@ -69,3 +69,21 @@ CREATE TABLE `blog_soup` (
 
 
 INSERT INTO blog_soup (`content`) VALUES ('你全心全力做到的最好\r\n可能还不如别人的随便搞搞');
+
+CREATE TABLE `blog_comment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `is_author` tinyint(1) NOT NULL DEFAULT '0',
+  `parent_id` int(10) unsigned DEFAULT NULL,
+  `root_id` int(10) unsigned DEFAULT NULL,
+  `article_id` int(10) unsigned NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `created_time` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `article` (`article_id`),
+  KEY `parent` (`parent_id`),
+  KEY `root` (`root_id`),
+  CONSTRAINT `root` FOREIGN KEY (`root_id`) REFERENCES `blog_comment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `article` FOREIGN KEY (`article_id`) REFERENCES `blog_article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `parent` FOREIGN KEY (`parent_id`) REFERENCES `blog_comment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
